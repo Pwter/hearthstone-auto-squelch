b34df827f30b95683c771afe4345cff63a9fa534
Pwter <dakcsi@gmail.com>
2016-05-28 23:01:22 +0200

    run as service, bubble notification

---
+++ node_modules/hearhstone-auto-squelcher/squelcher.ahk
@@ -0,0 +1,28 @@
+IfWinExist, Hearthstone
+{
+    SetFormat, float, 3.1
+
+    WinActivate, Hearthstone
+
+    WinWaitActive, Hearthstone
+    {
+        WinGetPos, hsx, hsy, hsw, hsh, Hearthstone
+        MouseGetPos, mposx, mposy
+
+        heroposx := hsw / 2
+        heroposy := hsh / 5.5
+
+
+        squelchx := heroposx - 120
+        squelchy := heroposy - 60
+
+        Click %heroposx%, %heroposy% right
+        Click %squelchx%, %squelchy%
+
+        MouseMove, mposx, mposy
+
+    }
+
+}
+
+Exit
--- squelchy.js
+++ squelchy.js
@@ -1,13 +1,37 @@
 var LogWatcher = require('./hearthstone-log-watcher');
 var exec = require('child_process').exec;
+const WindowsBalloon = require('node-notifier').WindowsBalloon;
+
+var notifier = new WindowsBalloon({
+  withFallback: false, // Try Windows Toast and Growl first?
+  customPath: void 0 // Relative path if you want to use your fork of notifu
+});
 
 var lw = new LogWatcher();
 
 lw.on('mulligan-end', function() {
     console.log('Mulligan ended. Time to squelch!');
-    exec('"squelcher.exe"', function callback(error, stdout, stderr){});
-
+    exec('"squelcher.ahk"', function callback(error, stdout, stderr){});
+    notifier.notify({
+      title: "Squelchy",
+      message: "SMOrcer silenced!",
+      sound: false, // true | false.
+      time: 5000, // How long to show balloon in ms
+      wait: false, // Wait for User Action against Notification
+    }, function(error, response) {
+      console.log(response);
+    });
 });
 
 lw.start();
 console.log('Emote protection on!');
+
+notifier.notify({
+  title: "Squelchy",
+  message: "Emotes shields up!",
+  sound: false, // true | false.
+  time: 5000, // How long to show balloon in ms
+  wait: false, // Wait for User Action against Notification
+}, function(error, response) {
+  console.log(response);
+});
